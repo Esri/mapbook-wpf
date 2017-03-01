5ddc9fde83c3281aae88d193ddc927f29d905159
Mara Stoica <mara8799@esri.com>
2017-02-28 18:10:34 -0800

    Added comments and fixed style in xaml

--- OfflineMapBook/OfflineMapBook/Views/App.xaml.cs
+++ OfflineMapBook/OfflineMapBook/Views/App.xaml.cs
@@ -1,16 +1,35 @@
-﻿using Esri.ArcGISRuntime;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
+﻿// <copyright file="App.cs" company="Esri">
+//      Copyright (c) 2017 Esri. All rights reserved.
+//
+//      Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//      You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//      Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//      See the License for the specific language governing permissions and
+//      limitations under the License.
+// </copyright>
+// <author>Mara Stoica</author>using Esri.ArcGISRuntime;
 namespace OfflineMapBook
 {
+    using System;
+    using System.Windows;
+    using Esri.ArcGISRuntime;
+
+    /// <summary>
+    /// The main application class
+    /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Code that runs when the app starts
+        /// </summary>
+        /// <param name="sender">sender control</param>
+        /// <param name="e">event args</param>
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             try
@@ -22,7 +41,7 @@ namespace OfflineMapBook
                 // Initialize the ArcGIS Runtime before any components are created.
                 ArcGISRuntimeEnvironment.Initialize();
 
-                // Standard level functionality can be enabled once the ArcGIS Runtime is initialized.                
+                // Standard level functionality can be enabled once the ArcGIS Runtime is initialized.
                 // To enable Standard level functionality you must either:
                 // 1. Allow the app user to authenticate with ArcGIS Online or Portal for ArcGIS then call the set license method with their license info.
                 // ArcGISRuntimeEnvironment.License.SetLicense(LicenseInfo object returned from an ArcGIS Portal instance)
--- OfflineMapBook/OfflineMapBook/Views/MapView.xaml.cs
+++ OfflineMapBook/OfflineMapBook/Views/MapView.xaml.cs
@@ -1,39 +1,65 @@
-﻿using OfflineMapBook.ViewModels;
-using System.Windows.Controls;
-
+﻿// <copyright file="BooleanToVisibilityInverseConverter.cs" company="Esri">
+//      Copyright (c) 2017 Esri. All rights reserved.
+//
+//      Licensed under the Apache License, Version 2.0 (the "License");
+//      you may not use this file except in compliance with the License.
+//      You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//      Unless required by applicable law or agreed to in writing, software
+//      distributed under the License is distributed on an "AS IS" BASIS,
+//      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//      See the License for the specific language governing permissions and
+//      limitations under the License.
+// </copyright>
+// <author>Mara Stoica</author>
 namespace OfflineMapBook
 {
-   
+    using System.Windows.Controls;
+    using ViewModels;
+
     /// <summary>
     /// Interaction logic for MapView.xaml
     /// </summary>
     public partial class MapView : UserControl
     {
-        MapViewModel ViewModel { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapView"/> class.
+        /// </summary>
         public MapView()
         {
-            InitializeComponent();
-            this.DataContextChanged += MapView_DataContextChanged;            
-        }
+            this.InitializeComponent();
 
-        private void MapView_DataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
-        {
-            if (this.DataContext != null)
+            // Set data context for the view
+            this.DataContextChanged += (s, e) =>
             {
-                ViewModel = this.DataContext as MapViewModel;
-                ViewModel.PropertyChanged += ViewModel_PropertyChanged;
-            }
+                if (this.DataContext != null)
+                {
+                    this.ViewModel = this.DataContext as MapViewModel;
+                    this.ViewModel.PropertyChanged += this.ViewModel_PropertyChanged;
+                }
+            };
         }
 
+        private MapViewModel ViewModel { get; set; }
+
+        /// <summary>
+        /// Property changed event handler for the view
+        /// </summary>
+        /// <param name="sender">Sender control</param>
+        /// <param name="e">event args</param>
         private async void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             switch (e.PropertyName)
             {
+                // When viewpoint changes, call SetViewpointAsync on the MapView
                 case "ViewPoint":
                     if (this.ViewModel.ViewPoint != null)
                     {
                         await this.MapBookMapView.SetViewpointAsync(this.ViewModel.ViewPoint);
                     }
+
                     break;
             }
         }
